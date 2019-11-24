e087a8087ee30a4c58e41dceb9365cde3a7e1a80
qavan <pro100qavan@gmail.com>
2019-11-24 18:58:59 +0300

    8 ready, vizualizer now can draw isolated nodes

--- main/vizualizer.py
+++ main/vizualizer.py
@@ -1,17 +1,21 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import random
 def vizualize_ver0(w):
     G = nx.Graph()
     for elem, value in w.items():
+        if value == []:
+            G.add_edge(str(elem), str(random.randint(1, len(w))), weight=0)
         for xvalue in value:
-            G.add_edge(str(elem), str(xvalue))
-            # G.add_edge(str(elem), str(xvalue), weight=(random.randint(10,100)/100))
-    # simple = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > 0.5]
-    pos = nx.spring_layout(G)  # positions for all nodes
+            G.add_edge(str(elem), str(xvalue), weight=1)
+    connected = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] == 1]
+    isolated = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] == 0]
+    pos = nx.spring_layout(G)
     nx.draw_networkx_nodes(G, pos, node_size=440, node_color='orange')
-    nx.draw_networkx_edges(G, pos, width=len(list(w.keys())), edge_color='b')
+    nx.draw_networkx_edges(G, pos, width=len(list(w.keys())), edge_color='b', edgelist=connected, alpha=1)
+    nx.draw_networkx_edges(G, pos, width=len(list(w.keys())), edge_color='b', edgelist=isolated, alpha=0)
     nx.draw_networkx_labels(G, pos, font_size=16, font_family='sans-serif', font_weight='bold')
     plt.axis('on')
     plt.show()
-    return
+    return True
 
