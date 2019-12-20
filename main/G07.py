24b8ce3c01f046e81b6448433a01c82a68ad8633
qavan <pro100qavan@gmail.com>
2019-12-20 21:01:14 +0300

    some fixes

--- main/G07.py
+++ main/G07.py
@@ -1,10 +1,3 @@
-import random
-from vizualizer import vizualize_ver0 as visualize
-from utilites import reprint as reprint
-from utilites import ms_ss as ms_ss
-from utilites import read_graph_from_ini as read_graph
-
-
 def is_graph_connected(xw: dict):
     def dfs(w: dict, cur_node: int, visited: list, cmpnts: list):
         nonlocal components
@@ -14,47 +7,43 @@ def is_graph_connected(xw: dict):
             if node not in visited:
                 dfs(w, node, visited, cmpnts)
     visit, components, node_list = [], 0, list(xw.keys())
-    cc = [0 for ni in range(len(node_list))]
+    visit_count = [0 for ni in range(len(node_list))]
     for xnode in node_list:
         if xnode not in visit:
             components += 1
-            dfs(xw, xnode, visit, cc)
-    if len(cc) == cc.count(1):
+            dfs(xw, xnode, visit, visit_count)
+    if len(visit_count)-1 == visit_count.count(1):
         return True
     return False
 
 
 def graph_component(xw: dict):
     def dfs(w: dict, cur_node: int):
-        nonlocal visited, pseudo_visited
+        nonlocal visited, lil_component
         visited.append(cur_node)
-        pseudo_visited.append(cur_node)
+        lil_component.append(cur_node)
         for node in w[cur_node]:
             if node not in visited:
                 dfs(w, node)
-        return visited
-    nodes = list(xw.keys())
+        return
+    node_list = list(xw.keys())
     visited = []
-    visiteds = []
-    # print('Список вершин графа:', nodes)
-    # print('Количество вершин:', len(nodes))
-    while visited != nodes:
-        pseudo_visited = []
-        notvisited = [node for node in nodes if node not in visited]
+    components = []
+    while visited != node_list:
+        lil_component = []
+        notvisited = [node for node in node_list if node not in visited]
         if notvisited == []:
             break
         dfs(xw, notvisited[0])
-        visiteds.append(pseudo_visited)
+        components.append(lil_component)
     component_max = 0
-    for counter, elem in enumerate(visiteds):
+    for counter, elem in enumerate(components):
         component_max = counter if len(elem) > component_max else component_max
-        # print(elem)
-    print('Число компонент связности графа -', len(visiteds))
-    print('Компонента связности графа -', visiteds[component_max])
-    print('Ниже перечислены все компоненты графа')
-    for elem in visiteds:
+    print('Число компонент связности графа: ', len(components))
+    print('Компонента связности графа: ', components[component_max])
+    print('Компоненты графа: ')
+    for elem in components:
         print(elem)
-
 
 def is_graph_bipartite_dfs(w: dict, cur_node: int, cur_node_color: int, color: list):
     color[cur_node-1] = cur_node_color
@@ -63,11 +52,7 @@ def is_graph_bipartite_dfs(w: dict, cur_node: int, cur_node_color: int, color: l
             return is_graph_bipartite_dfs(w, adj_node, cur_node_color * (-1), color)
         elif color[adj_node-1] == cur_node_color:
             return False
-    # print([counter+1 for counter, elem in enumerate(color) if elem == 1])
-    # print([counter+1 for counter, elem in enumerate(color) if elem == -1])
     return True
-
-
 def is_graph_have_cycle(xw):
     def is_cycle_util(w: dict, cur_node: int, visited: list, recursive: list):
         visited[cur_node-1] = True
@@ -126,7 +111,7 @@ def find_cycles(xw: dict):
                 indexes.append(point+iter)
                 iter += 1
             for x in range(1, len(indexes)):
-                # print(indexes[x], copyloop[indexes[x-1]:indexes[x]], copyloop, indexes)
+                print(indexes[x], copyloop[indexes[x-1]:indexes[x]], copyloop, indexes)
                 tmplist = copyloop[indexes[x-1]:indexes[x]]
                 # print(tmplist)
                 trpl = tmplist.copy()
@@ -153,6 +138,43 @@ def find_cycles(xw: dict):
 #     2: [],
 #     3: [],
 #     4: [5],
+# }
+# ez3 = {
+#     1: [2, 4],
+#     2: [1, 3, 4],
+#     3: [1, 2],
+#     4: [1]
+# }
+# ez4 = {
+#     1: [2, 3],
+#     2: [1],
+#     3: [1],
+#     4: [5],
+#     5: [4]
+# }
+
+# 1 проверить граф на связность
+# print('Граф связный') if is_graph_connected(ez1) else print('Граф не связный')
+# print('Граф связный') if is_graph_connected(ez2) else print('Граф не связный')
+# 2 компонента связности
+# graph_component(ez3)
+# graph_component(ez4)
+# 3 проверить граф на двудольность
+# v1 = [0 for ni in range(len(ez1))]
+# v2 = [0 for ni in range(len(ez2))]
+# print('Граф двудольный') if is_graph_bipartite_dfs(ez1, 1, 1, v1) else print('Граф не двудольный')
+# print('Граф двудольный') if is_graph_bipartite_dfs(ez2, 1, 1, v2) else print('Граф не двудольный')
+# ez1 = {
+#     1: [2],
+#     2: [3],
+#     3: [1],
+#     4: [1]
+# }
+# ez2 = {
+#     1: [2, 3],
+#     2: [],
+#     3: [],
+#     4: [5],
 #     5: []
 # }
 # ez3 = {
@@ -162,30 +184,6 @@ def find_cycles(xw: dict):
 #     4: [3],
 #     5: [4]
 # }
-# ez1 = {
-#     1: [2, 3, 4],
-#     2: [1, 3],
-#     3: [1, 2],
-#     4: [1]
-# }
-# ez2 = {
-#     1: [2, 3],
-#     2: [1],
-#     3: [1],
-#     4: [5],
-#     5: [4]
-# }
-# 1 проверить граф на связность
-# print('Граф связный') if is_graph_connected(ez1) else print('Граф не связный')
-# print('Граф связный') if is_graph_connected(ez2) else print('Граф не связный')
-# 2 компонента связности
-# graph_component(ez1)
-# graph_component(ez2)
-# 3 проверить граф на двудольность
-# v1 = [0 for ni in range(len(ez1))]
-# v2 = [0 for ni in range(len(ez2))]
-# print('Граф двудольный') if is_graph_bipartite_dfs(ez1, 1, 1, v1) else print('Граф не двудольный')
-# print('Граф двудольный') if is_graph_bipartite_dfs(ez2, 1, 1, v2) else print('Граф не двудольный')
 # 4 проверить граф на ацикличность
 # is_graph_have_cycle(ez1)
 # is_graph_have_cycle(ez2)
@@ -194,8 +192,3 @@ def find_cycles(xw: dict):
 # find_cycles(ez1)
 # find_cycles(ez2)
 # find_cycles(ez3)
-# ???
-# visualize(ez1)
-# visualize(ez2)
-# reprint(ez1)
-# reprint(ez2)
